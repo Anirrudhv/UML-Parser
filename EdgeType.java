7e961d4d92e5673738971154d3735ac642324740
Anirrudh <v.anirrudh@yahoo.in>
2017-04-06 11:55:10 -0700

    Update EdgeType.java

--- EdgeType.java
+++ EdgeType.java
@@ -1,10 +1,4 @@
-package edu.sjsu.cmpe202.pratiksanglikar.beans;
 
-/**
- * This enum represents various types of edges that can be drawn in a class diagram
- * @author pratiksanglikar
- *
- */
 public enum EdgeType {
 	EXTENDS, // TheEconomy extends ConcreteSubject => ConcreteSubject <|-- TheEconomy
 	IMPLEMENTS, // ConcreteSubject implements Subject =>  Subject <|.. ConcreteSubject
