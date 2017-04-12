482e9ca1736eb71e86aaeaf77a4df146028f20ac
Anirrudh <v.anirrudh@yahoo.in>
2017-04-11 19:12:22 -0700

    recreating the structure

--- EdgeType.java
+++
@@ -1,7 +0,0 @@
-
-public enum EdgeType {
-	EXTENDS, // TheEconomy extends ConcreteSubject => ConcreteSubject <|-- TheEconomy
-	IMPLEMENTS, // ConcreteSubject implements Subject =>  Subject <|.. ConcreteSubject
-	COMPOSITON, // ConcreteSubject has collection<Observer> => ConcreteSubject 1 - * Observer
-	ASSOCIATION // ..>
-}
